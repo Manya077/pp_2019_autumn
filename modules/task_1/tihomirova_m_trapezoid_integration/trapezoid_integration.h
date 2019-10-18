3c4a21144b897449b9d8385b3f7b1572c7546161
Manya077 <m.a.tihomirova.1999@yandex.ru>
2019-10-18 19:59:15 +0300

    first

---
+++ modules/task_1/tihomirova_m_trapezoid_integration/trapezoid_integration.h
@@ -0,0 +1,10 @@
+// Copyright 2019 Tihomirova Mariya
+#ifndef MODULES_TASK_1_TIHOMIROVA_M_TRAPEZOID_INTEGRATION_TRAPEZOID_INTEGRATION_H_
+#define MODULES_TASK_1_TIHOMIROVA_M_TRAPEZOID_INTEGRATION_TRAPEZOID_INTEGRATION_H_
+#include <mpi.h>
+
+double Trapezoid_area(double(*f)(double), double x1, double x2, double h);//нахождение площади трапеции
+double  Parallelization(double(*f)(double), double a, double b, int n);//распараллеливание
+
+#endif // MODULES_TASK_1_TIHOMIROVA_M_TRAPEZOID_INTEGRATION_TRAPEZOID_INTEGRATION_H_
+
